053d678d1396f91af0a41c44ca7e6a24be75afa9
OdyAuri <ody.loa100@gmail.com>
2026-05-13 19:50:15 -0600

    Create ejercicio2.c

---
+++ ejercicio2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define pi=3.14159
+int main () 
+{
+ int r, a, c;
+  printf("Escriba la magnitud del radio");
+  scanf("%d", &r);
+  a=pi*(r*r);
+  c= 2*pi*r;
+  printf("El area de la circunferencia es %d", &a);
+  printf("La circunferencia es %d", &c);
+return 0;
+}

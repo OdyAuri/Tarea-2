982c608dd18fb8452039844746206eb997b87122
OdyAuri <ody.loa100@gmail.com>
2026-05-16 12:43:48 -0600

    Update ejercicio3.c

--- ejercicio3.c
+++ ejercicio3.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 void main ()
 {
-double dol=0p=0;
+double d=0, p=0;
+  do 
+  {
   printf("Ingrese la cantidad de dolares a convertir \n");
-  scanf("%f", dol);
-  p = dol*11.96
-  printf("la cantidad de dolares en pesos es: %f", p);
+  scanf("%lf", &d);
+  }
+  while (d<=0);
+  p = d*11.96;
+  printf("la cantidad de dolares en pesos es: \n %lf", p);
 }
